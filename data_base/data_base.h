bdbf675ab10fb8885132eec8dc0a2e58d32824ef
Maksim Kl <GuruHacker@yandex.ru>
2024-04-03 23:25:37 +0300

    Initial commit

---
+++ data_base/data_base.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <pqxx/pqxx>
+#include <string>
+#include <map>
+
+class data_base {
+public:
+	data_base(const std::string& idb);
+	//data_base(const data_base& db);
+	//data_base() :conn_db_{ pqxx::connection() } {};
+	void add_keyword_link(const std::map<std::string, unsigned int>& words, const std::string& link);
+	bool check_url(const std::string& url);
+	bool check_keyword(const std::string& keyword);
+	std::map<std::string, int> get_urls_qry(const std::string& keyword);
+private:
+	pqxx::connection conn_db_;
+	void create_db_();
+};
